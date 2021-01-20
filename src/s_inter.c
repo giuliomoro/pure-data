giuliomoro/pure-data

commit e38a0db192667ca4bc7fa87400641003614b08b1

THREADED_IO: never select() from the audio thread. Trust the IO thread that the relevant fds are ready
Addresses @Spacechild1's https://github.com/pure-data/pure-data/pull/1261#pullrequestreview-570864733

src/s_inter.c
@@ -828,9 +828,10 @@ static int sys_domicrosleep(int microsec, int pollem)
                 }
             }
         }
-#endif // THREADED_IO
+#else // THREADED_IO
         fd_set readset;
         fdp_select(&readset, timout, kFdpManagerAudioThread);
+#endif // THREADED_IO
         pd_this->pd_inter->i_fdschanged = 0;
         for (i = 0; i < pd_this->pd_inter->i_nfdpoll &&
             !pd_this->pd_inter->i_fdschanged; i++)
@@ -838,18 +839,23 @@ static int sys_domicrosleep(int microsec, int pollem)
             t_fdpoll *fp = pd_this->pd_inter->i_fdpoll + i;
             int fd = fp->fdp_fd;
             int should_call = 0;
+#ifdef THREADED_IO
             t_fdp_manager fdp_manager = SYNC_FETCH(&fp->fdp_manager);
             if(kFdpManagerAudioThread == fdp_manager) {
+                should_call = 1;
+                didsomething = 1;
+            } else {
+                if(rbskt_ready(fp->fdp_rbskt))
+                    should_call = 1;
+            }
+#else // THREADED_IO
+            if(kFdpManagerAudioThread == fp->fdp_manager) {
                 if(FD_ISSET(fd, &readset)) {
                     should_call = 1;
                     didsomething = 1;
                 }
-            } else {
-#ifdef THREADED_IO
-                if(rbskt_ready(fp->fdp_rbskt))
-                    should_call = 1;
-#endif // THREADED_IO
             }
+#endif // THREADED_IO
             if(should_call)
             {
                 (*pd_this->pd_inter->i_fdpoll[i].fdp_fn)

commit bf2e29c89321f687944d60603cbfb4ee73a20353

THREADED_IO: ensure pointer is still valid if reallocation happens.
Fixes an occasional bug introduced in e38a0db192667ca4bc7fa87400641003614b08b1.

src/s_inter.c
@@ -868,11 +868,19 @@ static int sys_domicrosleep(int microsec, int pollem)
             !pd_this->pd_inter->i_fdschanged; i++)
         {
             t_fdpoll *fp = pd_this->pd_inter->i_fdpoll + i;
+            int fd = fp->fdp_fd;
+            // IMPORTANT: this fp is invalidated if i_fdschanged becomes ture
+            //after callpollfn().
 #ifdef THREADED_IO
             t_fdp_manager fdp_manager = SYNC_FETCH(&fp->fdp_manager);
             if(kFdpManagerAudioThread == fdp_manager) {
                 callpollfn(i);
                 didsomething = 1;
+                if(pd_this->pd_inter->i_fdschanged)
+                {
+                    // grab a fresh pointer
+                    fp = find_fdpoll(fd);
+                }
                 // restore ownership to IO thread
                SYNC_STORE(&fp->fdp_manager, kFdpManagerIoThread);
             } else {